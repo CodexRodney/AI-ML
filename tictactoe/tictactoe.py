32e7b07cf9eb0c743d67e2379db3b4f050e15f4f
CodexRodney <rodney002mwanje@gmail.com>
2023-06-23 16:31:31 +0300

    Feature: Implementation of minmax algorithm

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,7 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
-
+import copy
 X = "X"
 O = "O"
 EMPTY = None
@@ -56,9 +56,9 @@ def result(board, action):
     if isinstance(possible_actions, set):
         if action in possible_actions:
             # deep copy of original board
-            new_board = board[0:]
+            new_board = copy.deepcopy(board)
             # inserts the next player in the action set
-            new_board[action[0]][action[1]] = player(board)
+            new_board[action[0]][action[1]] = player(new_board)
             return new_board
         else:
             raise ValueError("Inappropriate Move")
@@ -104,9 +104,82 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+    #Assuming the role of a minimal player without playing optimally
     if terminal(board):
-        return None 
-    raise NotImplementedError
+        return None
+    print("The next player is ", player(board))
+    if player(board) == O:
+        move = min_player(board)[0]
+        print("The move is ", move)
+        return move
+    elif player(board) == X:
+        move = max_player(board)[0]
+        print("The move is ", move)
+        return move
+    # new_board = copy.deepcopy(board)
+    # value = -math.inf
+    # best_action = None
+    # for action in actions(new_board):
+    #     # ai to play move
+    #     new_board = result(new_board, action)
+    #     oponent_new_board = copy.deepcopy(new_board)
+    #     # taking account of the ai's move
+    #     for oponent_action in actions(oponent_new_board):
+    #         # taking into consideration oponent's move
+    #         print("The possible actions are: ", actions(oponent_new_board))
+    #         oponent_new_board = result(oponent_new_board, oponent_action)
+    #         utilize = utility(oponent_new_board)
+    #         if value < utilize:
+    #             # print("The Action: ", oponent_action)
+    #             # print("The value is : ", value)
+    #             best_action = oponent_action
+    #             value = utilize
+    #         oponent_new_board = new_board
+    #     new_board = board
+    # return best_action
+    
+def max_player(board):
+    # checks if the game is at the end
+    if terminal(board):
+        return utility(board)
+    
+    print("The board in max_player:")
+    print(board)
+    v = -math.inf # will hold the utility with the highest value
+    best_action = None # plays the best move to be
+    for action in actions(board):
+        print("Considering another move in the max player")
+        current_utility = min_player(result(board, action))
+        print(f"In the max player the utility is {current_utility}")
+        if isinstance(current_utility, tuple):
+            if current_utility[1] > v:
+                v = current_utility[1]
+                best_action = action
+        elif current_utility > v:
+            v = current_utility
+            best_action = action
+    return (best_action, v)
+        
+
+def min_player(board):
+    if terminal(board):
+        return utility(board)
+    
+    print("The board in min_player:")
+    print(board)
+    v = math.inf
+    best_action = None
+    for action in actions(board):
+        current_utility = max_player(result(board, action))
+        print(f"In the min player the utility is {current_utility}")
+        if isinstance(current_utility, tuple):
+            if current_utility[1] < v:
+                v = current_utility[1]
+                best_action = action
+        elif current_utility < v:
+            v = current_utility
+            best_action = action
+    return (best_action, v)
 
 def diagonal_win(board, character):
     """
@@ -138,6 +211,8 @@ def horizontal_winning(board, character):
     Returns True if character occupies any of 
     all the horizontal positions of board
     """
+    # print("The board being checked is\n", board)
+    # print("The Character being checked is ", character)
     for x in board:
         count = 0
         for w in x:
@@ -160,4 +235,3 @@ def vertical_winning(board, character):
             return True
     return False
 
-# def max 
